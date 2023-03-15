66fbb43ae302e6c321f2ffade833e7b7a1603e86
Sreejith Kartha <kartha4u@gmail.com>
2023-03-15 17:07:30 -0500

    process.env.PORT to supply port by AWS

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000 // Port will be picked by AWS
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World from EBS!')
 })
 
 app.listen(port, () => {
